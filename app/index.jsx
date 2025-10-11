79c58f5916b4f269e5202543e77bdb82ccc9f5ee
OneSummerDay <artem.kraievyi@gmail.com>
2025-10-11 19:42:17 +0300

    feat: add new components and implement styling

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,38 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>The number 1</Text>
+      <Text>The number 2</Text>
+
+      <View>
+        <Text style={styles.card}>
+            Hello, this is a card component!
+        </Text>
+    </View>
+
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 18,
+    },
+    card: {
+        backgroundColor: "#eee",
+        padding: 10,
+        borderRadius: 5,
+    },
+
+})
